0cb0fecbef5f5a5fc26fa8312be3cb21f4720b78
rayanne294 <53279147+rayanne294@users.noreply.github.com>
2020-11-20 20:56:10 -0300

    Add files via upload

--- js/java.js
+++ js/java.js
@@ -7,8 +7,8 @@ const jogos = [
     {nome: 'guitar hero',img:'jogos/gui.png',desc:'Guitar Hero 3 Legends of Rock-', plataforma:'Xbox 360',valor:'R$100,00 '},
     {nome: 'guitar hero',img:'jogos/guitar.png',desc:'Guitar Hero Live-', plataforma:'Xbox One',valor:'R$100,00 '},
     {nome: 'homem aranha',img:'jogos/homearanha.png',desc:'Spider Man-', plataforma:'PS5',valor:'R$100,00 '},
-    {nome: 'Red Dead Redemption',img:'jogos/red.png',desc:'Red Dead Redemption 2-', plataforma:'Xbox One',valor:'R$100,00 '},
-    {nome: 'tony',img:'jogos/th.png',desc:"Tony Hawk's Pro Skater 1+2-", plataforma:'PS4',valor:'R$100,00 '}
+    {nome: 'red Dead Redemption',img:'jogos/red.png',desc:'Red Dead Redemption 2-', plataforma:'Xbox One',valor:'R$100,00 '},
+    {nome: 'skate',img:'jogos/th.png',desc:"Tony Hawk's Pro Skater 1+2-", plataforma:'PS4',valor:'R$100,00 '}
 ]
 
 
--- js/jogos.js
+++ js/jogos.js
@@ -1,6 +1,8 @@
 const busca = document.querySelector("#txtBusca")
 const game = document.querySelector('#game')
+const filtro = document.querySelector('#place')
 
+exibirTudo()
 function buscarNome(x) {
     let conc = '' 
     for (let i of jogos){
@@ -14,15 +16,33 @@ function buscarNome(x) {
 function exibirTudo() {
     let y = ''
     for (let i of jogos) {
-       y += `<div><img class = "jogo" src = "${i.img}"><p>${i.desc} ${i.plataforma} <br>${i.valor}</p></div>`
+        y += `<div><img class = "jogo" src = "${i.img}"><p>${i.desc} ${i.plataforma} <br>${i.valor}</p></div>`
     }           
     game.innerHTML = y
 }
 
-busca.addEventListener('keyup', function(event) {
-    if (event.key == 'Enter') {
-        buscarNome(busca.value)
-        
-}
+
+busca.addEventListener('keypress', function(event) {
+       buscarNome(busca.value)
     
+})
+
+
+function filtrar(plat) {
+    let b = ''
+    if (filtro.value == 'Todos'){
+        exibirTudo()
+        return
+    }
+    for (let i of jogos){
+        if (i.plataforma.includes(plat)){
+        b += `<div><img class = "jogo" src = "${i.img}"><p>${i.desc} ${i.plataforma} <br>${i.valor}</p></div>`
+    }
+    
+    }
+    game.innerHTML = b
+}
+filtro.addEventListener('click',function(){
+    filtrar(filtro.value)
+
 })
